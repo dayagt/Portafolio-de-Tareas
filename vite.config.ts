54d34c65f2fd6aa11e044237eaa39b97e8734cfe
Gelen Dayanna López Morales <dayannaldayamorales2125@gmail.com>
2025-10-20 08:12:54 -0600

    Configuración para publicar en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/Portafolio-de-Tareas/', // 👈 nombre exacto de tu repositorio
+});
